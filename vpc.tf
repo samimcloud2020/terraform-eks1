4261b7268c0c5678ca11bd376e34a72bd66dcbd6
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2023-01-21 13:37:27 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "samim_main" {
 
 resource "aws_subnet" "public_subnets" {
  count             = length(var.public_subnet_cidrs)
- vpc_id            = aws_vpc.main.id
+ vpc_id            = aws_vpc.samim_main.id
  cidr_block        = element(var.public_subnet_cidrs, count.index)
  availability_zone = element(var.azs, count.index)
  
@@ -20,7 +20,7 @@ resource "aws_subnet" "public_subnets" {
  
 resource "aws_subnet" "private_subnets" {
  count             = length(var.private_subnet_cidrs)
- vpc_id            = aws_vpc.main.id
+ vpc_id            = aws_vpc.samim_main.id
  cidr_block        = element(var.private_subnet_cidrs, count.index)
  availability_zone = element(var.azs, count.index)
  
