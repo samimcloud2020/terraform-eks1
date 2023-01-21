60a25a573518f9818fe1a76a2ec26376f5d21a1c
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2023-01-21 13:17:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,3 +20,10 @@ variable "private_subnet_cidrs" {
  description = "Private Subnet CIDR values"
  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
+
+variable "azs" {
+ type        = list(string)
+ description = "Availability Zones"
+ default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
