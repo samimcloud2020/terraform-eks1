b45c50ba5554767d4c4049aa3639a00a441050f9
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2023-01-21 13:16:08 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,108 +7,45 @@ resource "aws_vpc" "samim_vpc" {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-resource "aws_vpc" "samim_main" {
-  cidr_block       = "192.168.0.0/16"
-  
-  tags = {
-    Name = "samim_vpc"
-  }
+resource "aws_subnet" "public_subnets" {
+ count      = length(var.public_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.public_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Public Subnet ${count.index + 1}"
+ }
+}
+ 
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Private Subnet ${count.index + 1}"
+ }
 }
 
 
-data "aws_availability_zones" "available" {
-  state = "available"
-}
 
 
-resource "aws_subnet" "samim_subnet1" {
-  vpc_id     = aws_vpc.samim_main.id
-  availability_zone = data.aws_availability_zones.available.names[0]
-  cidr_block = "192.168.1.0/24"
-
-  tags = {
-    Name = "samim_subnet1"
-  }
-}
-
-resource "aws_subnet" "samim_subnet2" {
-  vpc_id     = aws_vpc.samim_main.id
-  availability_zone = data.aws_availability_zones.available.names[1]
-  cidr_block = "192.168.2.0/24"
-
-  tags = {
-    Name = "samim_subnet2"
-  }
-}
-
-resource "aws_subnet" "samim_subnet3" {
-  vpc_id     = aws_vpc.samim_main.id
-  availability_zone = data.aws_availability_zones.available.names[2]
-  cidr_block = "192.168.3.0/24"
-
-  tags = {
-    Name = "samim_subnet3"
-  }
-}
-
-resource "aws_subnet" "samim_subnet4" {
-  vpc_id     = aws_vpc.samim_main.id
-  availability_zone = data.aws_availability_zones.available.names[3]
-  cidr_block = "192.168.4.0/24"
-
-  tags = {
-    Name = "samim_subnet4"
-  }
-}
 
 
-resource "aws_subnet" "samim_subnet5" {
-  vpc_id     = aws_vpc.samim_main.id
-  availability_zone = data.aws_availability_zones.available.names[4]
-  cidr_block = "192.168.5.0/24"
-
-  tags = {
-    Name = "samim_subnet5"
-  }
-}
 
 
-resource "aws_internet_gateway" "samim_igw" {
-  vpc_id = aws_vpc.samim_main.id
 
-  tags = {
-    Name = "samim_igw"
-  }
-}
 
-resource "aws_internet_gateway_attachment" "samim_igwa" {
-  internet_gateway_id = aws_internet_gateway.samim_igw.id
-  vpc_id              = aws_vpc.samim_main.id
-  
-  tags = {
-    Name = "samim_igwa"
-  }
-}
+
+
+
+
+
+
+
+
+
+
+
+
 
