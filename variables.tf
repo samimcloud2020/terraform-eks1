514ae6883246754e2b10888e8183b2afd1019f9e
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2023-01-21 13:14:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,16 @@ variable "subnet_id_1" {
   type = string
   default = "subnet-your_second_subnet_id"
  }
+
+
+variable "public_subnet_cidrs" {
+ type        = list(string)
+ description = "Public Subnet CIDR values"
+ default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+ 
+variable "private_subnet_cidrs" {
+ type        = list(string)
+ description = "Private Subnet CIDR values"
+ default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
