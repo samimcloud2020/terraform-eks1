5228a213f048e1477d4a6d5411c3d2f6e4f7e3e0
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2023-01-21 13:45:17 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -41,27 +41,10 @@ resource "aws_internet_gateway" "igw" {
 
 
 
-resource "aws_internet_gateway_attachment" "igwa" {
-  internet_gateway_id = aws_internet_gateway.igwa.id
-  vpc_id              = aws_vpc.samim_main.id
- 
- 
-}
 
 
 
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.samim_main.id
 
-  route {
-    cidr_block = element(var.public_subnet_cidrs, count.index)
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "samim_rt"
-  }
-}
 
 
 
